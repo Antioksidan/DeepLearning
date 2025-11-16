667a4466d8cc6dd817f1baebadc532a6c4aee96a
VinceSzvi94 <szigetvari.vince@gmail.com>
2025-11-16 16:45:51 +0100

    test bicubic baseline

---
+++ src/dataloader.py
@@ -0,0 +1,103 @@
+import os
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import torchvision.transforms as transforms
+
+class DIV2KDataset(Dataset):
+    def __init__(self, lr_dir, hr_dir, transform=None):
+        """
+        Args:
+            lr_dir: Directory with low-resolution images (64x64)
+            hr_dir: Directory with high-resolution images (256x256)
+            transform: Optional transform to be applied on images
+        """
+        self.lr_dir = lr_dir
+        self.hr_dir = hr_dir
+        self.transform = transform
+        
+        # Get list of image files
+        self.lr_images = sorted(os.listdir(lr_dir))
+        self.hr_images = sorted(os.listdir(hr_dir))
+        
+        # Ensure same number of images
+        assert len(self.lr_images) == len(self.hr_images), "Mismatch in number of LR and HR images"
+    
+    def __len__(self):
+        return len(self.lr_images)
+    
+    def __getitem__(self, idx):
+        # Load LR and HR images
+        lr_path = os.path.join(self.lr_dir, self.lr_images[idx])
+        hr_path = os.path.join(self.hr_dir, self.hr_images[idx])
+        
+        lr_image = Image.open(lr_path).convert('RGB')
+        hr_image = Image.open(hr_path).convert('RGB')
+        
+        if self.transform:
+            lr_image = self.transform(lr_image)
+            hr_image = self.transform(hr_image)
+        
+        return lr_image, hr_image
+
+class DIV2KDataModule:
+    def __init__(self, lr_dir='Dataset', hr_dir='Dataset', batch_size=16, num_workers=4):
+        """
+        Args:
+            lr_dir: Directory with low-resolution images
+            hr_dir: Directory with high-resolution images
+            batch_size: Number of samples per batch
+            num_workers: Number of subprocesses to use for data loading
+        """
+        self.lr_dir = lr_dir
+        self.hr_dir = hr_dir
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self):
+        self.train_dataset = DIV2KDataset(
+            lr_dir=os.path.join(self.lr_dir, 'train'),
+            hr_dir=os.path.join(self.hr_dir, 'train_labels'),
+            transform=self.transform
+        )
+        
+        self.val_dataset = DIV2KDataset(
+            lr_dir=os.path.join(self.lr_dir, 'validation'),
+            hr_dir=os.path.join(self.hr_dir, 'validation_labels'),
+            transform=self.transform
+        )
+        
+        self.test_dataset = DIV2KDataset(
+            lr_dir=os.path.join(self.lr_dir, 'test'),
+            hr_dir=os.path.join(self.hr_dir, 'test_labels'),
+            transform=self.transform
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
--- src/vgg_wrapper.py
+++ src/vgg_wrapper.py
@@ -2,3 +2,36 @@ import torch
 import torch.nn as nn
 from torchvision import models
 import numpy as np
+
+class VGGFeatureExtractor(nn.Module):
+    def __init__(self):
+        super(VGGFeatureExtractor, self).__init__()
+        
+        # Load a pre-trained VGG19 model
+        vgg19 = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
+        
+        # We want the features from the 36th layer (conv3_5, before the 4th maxpool)
+        # You can also use layer 9 (conv2_2) or 18 (conv3_3)
+        # In the SRGAN paper, they use conv_5_4 (layer 36), let's use that.
+        self.features = nn.Sequential(*list(vgg19.features.children())[:36]).eval()
+        
+        # Freeze the VGG model
+        for param in self.features.parameters():
+            param.requires_grad = False
+            
+        # VGG networks are trained on ImageNet, which has mean=[0.485, 0.456, 0.406]
+        # and std=[0.229, 0.224, 0.225]. We need to normalize our images
+        # before passing them to VGG.
+        self.normalize = nn.functional.normalize
+
+    def forward(self, x):
+        # Normalize the input image to match VGG's training data
+        # Note: This assumes your input images are in the range [0, 1].
+        # If your images are in [-1, 1], you'll need to adjust.
+        # Let's assume input is [0, 1] for simplicity.
+        x_normalized = x.clone()
+        x_normalized[:, 0] = (x[:, 0] - 0.485) / 0.229
+        x_normalized[:, 1] = (x[:, 1] - 0.456) / 0.224
+        x_normalized[:, 2] = (x[:, 2] - 0.406) / 0.225
+        
+        return self.features(x_normalized)
